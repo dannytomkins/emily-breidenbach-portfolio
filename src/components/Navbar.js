e18dcb5314a8e7033f990d29e7c65638847523d1
Danny Tomkins <tomkinsdan@gmail.com>
2022-07-26 18:48:36 -0500

    updated styling at navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,42 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const navbarStyle = {
+    // "width":"100%",
+    background: 'red',
+    display: 'flex',
+    // "flexWrap":"wrap",
+    justifyContent: 'space-between',
+  };
+  const navHomeStyle = {
+    'align-self': 'flex-start',
+    padding: '4px',
+  };
+  const navListStyle = {
+    listStyle: 'none',
+    padding: '4px',
+    display: 'flex',
+    justifyContent: 'space-between',
+  };
   return (
     <>
-    <div>Navbar</div>
-    <ul>
-      <li>
-        <Link to='/'>Meeb</Link>
-      </li>
-      <li>
-        <Link to='/classes'>CLASSES</Link>
-      </li>
-      <li>
-        <Link to='/student-work'>STUDENT WORK</Link>
-      </li>
-    </ul>
-    <div>Navbar end</div>
+      <div style={navbarStyle}>
+        <Link to='/'>Emily Briedenbach</Link>
+        <ul style={navListStyle}>
+          <li>
+            <Link to='/'>ABOUT</Link>
+          </li>
+          <li>
+            <Link to='/classes'>CLASSES</Link>
+          </li>
+          <li>
+            <Link to='/student-work'>STUDENT WORK</Link>
+          </li>
+        </ul>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
