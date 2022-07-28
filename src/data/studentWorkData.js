6585299ce56a80e9b83b177349f1f6adf3c52d69
Danny Tomkins <tomkinsdan@gmail.com>
2022-07-27 20:55:51 -0500

    updated image filenames and student work data

--- src/data/studentWorkData.js
+++ src/data/studentWorkData.js
@@ -9,6 +9,32 @@ import studentWork6 from '../img/cynthia-jaworski-3.jpg'
 import studentWork7 from '../img/cynthia-jaworski-4.jpg'
 import studentWork8 from '../img/cynthia-jaworski-5.jpg'
 import studentWork9 from '../img/cynthia-jaworski.jpg'
+import studentWork10 from '../img/daisy-wong-2.jpg'
+import studentWork11 from '../img/david-aitchinson.jpg'
+import studentWork12 from '../img/diana-frurip-2.jpg'
+import studentWork13 from '../img/diana-frurip-3.jpg'
+import studentWork14 from '../img/diana-frurip-4.jpg'
+import studentWork15 from '../img/diana-frurip-5.jpg'
+import studentWork16 from '../img/diana-frurip.jpg'
+import studentWork17 from '../img/erin-fairhead-2.jpg'
+import studentWork18 from '../img/erin-fairhead-3.jpg'
+import studentWork19 from '../img/jasmyne-robertson.jpg'
+import studentWork20 from '../img/cynthia-jaworski.jpg'
+import studentWork21 from '../img/cynthia-jaworski.jpg'
+import studentWork22 from '../img/cynthia-jaworski.jpg'
+import studentWork23 from '../img/cynthia-jaworski.jpg'
+import studentWork24 from '../img/cynthia-jaworski.jpg'
+import studentWork25 from '../img/cynthia-jaworski.jpg'
+import studentWork26 from '../img/cynthia-jaworski.jpg'
+import studentWork27 from '../img/cynthia-jaworski.jpg'
+import studentWork28 from '../img/cynthia-jaworski.jpg'
+import studentWork29 from '../img/cynthia-jaworski.jpg'
+import studentWork30 from '../img/cynthia-jaworski.jpg'
+import studentWork31 from '../img/cynthia-jaworski.jpg'
+import studentWork32 from '../img/cynthia-jaworski.jpg'
+import studentWork33 from '../img/cynthia-jaworski.jpg'
+import studentWork34 from '../img/cynthia-jaworski.jpg'
+import studentWork35 from '../img/cynthia-jaworski.jpg'
 
 
 export const studentWorkData = [
@@ -16,147 +42,245 @@ export const studentWorkData = [
         id: 1,
         name: "firstImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
+        description: "Description of Img",
         img: studentWork1
     },
     {
         id: 2,
         name: "secondImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
+        description: "Description of Img",
         img: studentWork2
     },
     {
         id: 3,
         name: "thirdImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
+        description: "Description of Img",
         img: studentWork3
     },
     {
         id: 4,
         name: "fourthImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
+        description: "Description of Img",
         img: studentWork4
     },
     {
         id: 5,
         name: "fifthImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
+        description: "Description of Img",
         img: studentWork5
     },
     {
         id: 6,
         name: "firstImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
+        description: "Description of Img",
         img: studentWork6
     },
     {
         id: 7,
         name: "firstImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
+        description: "Description of Img",
         img: studentWork7
     },
     {
         id: 8,
         name: "firstImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
+        description: "Description of Img",
         img: studentWork8
     },
     {
         id: 9,
         name: "firstImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
+        description: "Description of Img",
         img: studentWork9
     },
     {
         id: 10,
         name: "firstImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
-        img: defaultImg2
+        description: "Description of Img",
+        img: studentWork10
     },
     {
         id: 11,
         name: "firstImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
-        img: defaultImg1
+        description: "Description of Img",
+        img: studentWork11
     },
     {
         id: 12,
         name: "firstImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
-        img: defaultImg2
+        description: "Description of Img",
+        img: studentWork12
     },
     {
         id: 13,
         name: "firstImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
-        img: defaultImg1
+        description: "Description of Img",
+        img: studentWork13
     },
     {
         id: 14,
         name: "firstImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
-        img: defaultImg1
+        description: "Description of Img",
+        img: studentWork14
     },
     {
         id: 15,
         name: "firstImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
-        img: defaultImg1
+        description: "Description of Img",
+        img: studentWork15
     },
     {
         id: 16,
         name: "firstImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
-        img: defaultImg1
+        description: "Description of Img",
+        img: studentWork16
     },
     {
         id: 17,
         name: "firstImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
-        img: defaultImg2
+        description: "Description of Img",
+        img: studentWork17
     },
     {
         id: 18,
         name: "firstImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
-        img: defaultImg1
+        description: "Description of Img",
+        img: studentWork18
     },
     {
         id: 19,
         name: "firstImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
-        img: defaultImg2
+        description: "Description of Img",
+        img: studentWork19
     },
     {
         id: 20,
         name: "firstImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
-        img: defaultImg1
+        description: "Description of Img",
+        img: studentWork20
     },
     {
         id: 21,
         name: "firstImg",
         artistName: "Artist Name Here",
-        description: "Description of first Img",
-        img: defaultImg1
+        description: "Description of Img",
+        img: studentWork21
+    },
+    {
+        id: 22,
+        name: "firstImg",
+        artistName: "Artist Name Here",
+        description: "Description of Img",
+        img: studentWork22
+    },
+    {
+        id: 23,
+        name: "firstImg",
+        artistName: "Artist Name Here",
+        description: "Description of Img",
+        img: studentWork23
+    },
+    {
+        id: 24,
+        name: "firstImg",
+        artistName: "Artist Name Here",
+        description: "Description of Img",
+        img: studentWork24
+    },
+    {
+        id: 25,
+        name: "firstImg",
+        artistName: "Artist Name Here",
+        description: "Description of Img",
+        img: studentWork25
+    },
+    {
+        id: 26,
+        name: "firstImg",
+        artistName: "Artist Name Here",
+        description: "Description of Img",
+        img: studentWork26
+    },
+    {
+        id: 27,
+        name: "firstImg",
+        artistName: "Artist Name Here",
+        description: "Description of Img",
+        img: studentWork27
+    },
+    {
+        id: 28,
+        name: "firstImg",
+        artistName: "Artist Name Here",
+        description: "Description of Img",
+        img: studentWork28
+    },
+    {
+        id: 29,
+        name: "firstImg",
+        artistName: "Artist Name Here",
+        description: "Description of Img",
+        img: studentWork29
+    },
+    {
+        id: 30,
+        name: "firstImg",
+        artistName: "Artist Name Here",
+        description: "Description of Img",
+        img: studentWork30
+    },
+    {
+        id: 31,
+        name: "firstImg",
+        artistName: "Artist Name Here",
+        description: "Description of Img",
+        img: studentWork31
+    },
+    {
+        id: 32,
+        name: "firstImg",
+        artistName: "Artist Name Here",
+        description: "Description of Img",
+        img: studentWork32
+    },
+    {
+        id: 33,
+        name: "firstImg",
+        artistName: "Artist Name Here",
+        description: "Description of Img",
+        img: studentWork33
+    },
+    {
+        id: 34,
+        name: "firstImg",
+        artistName: "Artist Name Here",
+        description: "Description of Img",
+        img: studentWork34
+    },
+    {
+        id: 35,
+        name: "firstImg",
+        artistName: "Artist Name Here",
+        description: "Description of Img",
+        img: studentWork35
     },
 ]
