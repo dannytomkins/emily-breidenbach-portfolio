8600642598fa252407a1fa86bf8a48a46d7715c0
Danny Tomkins <tomkinsdan@gmail.com>
2022-07-29 19:25:01 -0500

    refactored styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  const navbarStyle = {
-    background: 'red',
-    display: 'flex',
-    justifyContent: 'space-between',
-  };
   const navListStyle = {
     listStyle: 'none',
     padding: '5px',
@@ -14,26 +9,24 @@ const Navbar = () => {
     justifyContent: 'space-between',
   };
   return (
-    <>
-      <div style={navbarStyle}>
+    <nav class="navbar">
       <ul style={navListStyle}>
         <li>
-        <Link to='/'>Emily Briedenbach</Link>
+          <Link to='/'>Emily Briedenbach</Link>
         </li>
-        </ul>
-        <ul style={navListStyle}>
-          <li>
-            <Link to='/'>ABOUT</Link>
-          </li>
-          <li>
-            <Link to='/classes'>CLASSES</Link>
-          </li>
-          <li>
-            <Link to='/student-work'>STUDENT WORK</Link>
-          </li>
-        </ul>
-      </div>
-    </>
+      </ul>
+      <ul style={navListStyle}>
+        <li>
+          <Link to='/'>ABOUT</Link>
+        </li>
+        <li>
+          <Link to='/classes'>CLASSES</Link>
+        </li>
+        <li>
+          <Link to='/student-work'>STUDENT WORK</Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
