e2cd33dfab05d3c66773b2708e0aa8aabec63465
Danny Tomkins <tomkinsdan@gmail.com>
2022-07-29 19:52:44 -0500

    updated nav styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  const navListStyle = {
-    listStyle: 'none',
-    padding: '5px',
-    display: 'flex',
-    justifyContent: 'space-between',
-  };
+
   return (
-    <nav class="navbar">
-      <ul style={navListStyle}>
+    <nav className='navbar'>
+      <ul className='navlist'>
         <li>
           <Link to='/'>Emily Briedenbach</Link>
         </li>
       </ul>
-      <ul style={navListStyle}>
+      <ul className='navlist'>
         <li>
           <Link to='/'>ABOUT</Link>
         </li>
