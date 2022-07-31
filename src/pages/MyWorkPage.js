cd07473c7f8c118679d3887daf8fec2357fe3aa4
Danny Tomkins <tomkinsdan@gmail.com>
2022-07-31 18:23:08 -0500

    created MyWorkPage

---
+++ src/pages/MyWorkPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MyWorkGallery from '../components/StudentWorkGallery'
+
+const MyWorkPage = () => {
+  return (
+    <div>
+    <MyWorkGallery />
+    </div>
+  )
+}
+
+export default MyWorkPage
