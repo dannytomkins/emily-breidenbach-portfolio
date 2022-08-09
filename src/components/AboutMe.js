0967d6890438996ddcda456943d1cc39356f9576
Danny Tomkins <tomkinsdan@gmail.com>
2022-08-08 21:03:09 -0500

    updated image styles

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,17 +1,24 @@
-import React from 'react'
-import myWork029 from '../img/my-work/my-work-029.jpg'
+import React from 'react';
+import myWork029 from '../img/my-work/my-work-029.jpg';
 
 const AboutMe = () => {
   return (
     <>
-    <div className='banner-image'>
-    <img src={myWork029}/>
-    </div>
-    <div className='text-block'>
-      <p>Hi there! I am a weaver based in Chicago, IL. I have a B.F.A. from the University of Illinois at Urbana-Champaign and an M.A. from the School of the Art Institute of Chicago. I love interacting with all levels of students and currently teach in the Adult Continuing Education program at the School of the Art Institute of Chicago as well as the botanic arts program at the Chicago Botanic Garden.</p>
-    </div>
+      <div className='banner-image'>
+        <img src={myWork029} />
+      </div>
+      <div className='text-block'>
+        <p>
+          Hi there! I am a weaver based in Chicago, IL. I have a B.F.A. from the
+          University of Illinois at Urbana-Champaign and an M.A. from the School
+          of the Art Institute of Chicago. I love interacting with all levels of
+          students and currently teach in the Adult Continuing Education program
+          at the School of the Art Institute of Chicago as well as the botanic
+          arts program at the Chicago Botanic Garden.
+        </p>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default AboutMe
+export default AboutMe;
