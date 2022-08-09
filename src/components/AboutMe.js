53067e95745aa9117a63dded92dab9cb30069292
Danny Tomkins <tomkinsdan@gmail.com>
2022-08-08 20:21:18 -0500

    updated about text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,7 +8,7 @@ const AboutMe = () => {
     <img src={myWork029}/>
     </div>
     <div className='text-block'>
-      <p>Hello. I am Emily :) Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam animi porro possimus odit ad a delectus, iusto, nam corrupti asperiores accusamus consequatur ab numquam accusantium explicabo. Tempore, necessitatibus? Dignissimos, voluptas.</p>
+      <p>Hi there! I am a weaver based in Chicago, IL. I have a B.F.A. from the University of Illinois at Urbana-Champaign and an M.A. from the School of the Art Institute of Chicago. I love interacting with all levels of students and currently teach in the Adult Continuing Education program at the School of the Art Institute of Chicago as well as the botanic arts program at the Chicago Botanic Garden.</p>
     </div>
     </>
   )
