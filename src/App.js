51a166ff7a395d343a196d09333c0b836862712c
Danny Tomkins <tomkinsdan@gmail.com>
2022-08-21 21:26:23 -0500

    removed unused imports and commented out html from App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import React, { Fragment, useEffect } from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import './App.css';
-import Navbar from './components/Navbar';
-import ClassesPage from './pages/ClassesPage';
-import LandingPage from './pages/LandingPage';
-import StudentWorkPage from './pages/StudentWorkPage';
 
 const App = () => {
-  // return (
-  //   <Router>
-  //       <Navbar />
-  //       <Routes>
-  //         <Route path='/' element={<LandingPage />} />
-  //         <Route path='/classes' element={<ClassesPage />} />
-  //         <Route path='/student-work' element={<StudentWorkPage />} />
-  //       </Routes>
-  //   </Router>
-  // );
 };
 
 export default App;
