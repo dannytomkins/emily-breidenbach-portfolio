b2f4ab3e973358315b7566ea8ef1ca075c0019c0
Danny Tomkins <tomkinsdan@gmail.com>
2022-11-17 19:51:45 -0600

    update about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,9 +9,7 @@ const AboutMe = () => {
       </div>
       <div className='text-block'>
         <p>
-          Hi there! I am a weaver based in Chicago, IL. I have a B.F.A. from the
-          University of Illinois at Urbana-Champaign and an M.A. from the School
-          of the Art Institute of Chicago. I love interacting with all levels of
+          Hi there! I am a weaver based in Chicago, IL. I love interacting with all levels of
           students and currently teach in the Adult Continuing Education program
           at the School of the Art Institute of Chicago as well as the botanic
           arts program at the Chicago Botanic Garden.
--- src/components/ArtWorkItem.js
+++ src/components/ArtWorkItem.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
 
 const ArtWorkItem = (props) => {
 
   return (
     <div>
+      <HashRouter>
+        <Routes>
+          <Route path=''>
       <img src={props.item.img} className='student-work-item'/>
+          </Route>
+        </Routes>
+      </HashRouter>
     </div>
   )
 }
