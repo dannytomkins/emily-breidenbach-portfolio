1c4fae16e08ccfcd861442bfba1f7728157300cb
Danny Tomkins <tomkinsdan@gmail.com>
2023-02-09 20:08:28 -0600

    improved navbar to include drop down while on mobile

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'
+import './Navbar.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <nav className='navbar'>
-      <ul className='navlist'>
-        <li id='nav-main'>
-          <Link to='/'>Emily Breidenbach</Link>
-        </li>
-      </ul>
-      <ul className='navlist'>
-        <li>
-          <Link to='/'>ABOUT</Link>
-        </li>
-        <li>
-          <Link to='/work'>PORTFOLIO</Link>
-        </li>
-        <li>
-          <Link to='/classes'>CLASSES</Link>
-        </li>
-        <li>
-          <Link to='/student-work'>STUDENT WORK</Link>
-        </li>
-      </ul>
+      <Link to='/' className='logo'>
+        Emily Breidenbach
+      </Link>
+
+      <div className='links'>
+        <Link to='/'>ABOUT</Link>
+
+        <Link to='/work'>PORTFOLIO</Link>
+
+        <Link to='/classes'>CLASSES</Link>
+
+        <Link to='/student-work'>STUDENT WORK</Link>
+      </div>
+
+      <div className="icon" onClick={handleToggle}>
+        <span>{isOpen ? "\u2715" : "\u2630"}</span>
+      </div>
+      {isOpen && (
+        <div className="dropdown-content">
+        <Link to='/' onClick={handleToggle}>ABOUT</Link>
+        <Link to='/work' onClick={handleToggle}>PORTFOLIO</Link>
+        <Link to='/classes' onClick={handleToggle}>CLASSES</Link>
+        <Link to='/student-work' onClick={handleToggle}>STUDENT WORK</Link>
+        </div>
+      )}
     </nav>
   );
 };
